81842f1058bbcb981221d7687e30883d51b15991
lamdersar <klam@f1v.co>
2014-03-29 17:02:59 -0400

    auth token done

--- src/moms/app/android/Login.java
+++ src/moms/app/android/Login.java
@@ -1,28 +1,136 @@
 package moms.app.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.savagelook.android.UrlJsonAsyncTask;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 /**
  * Created by klam on 3/29/14.
  */
 public class Login extends Activity {
+    private String mUserEmail;
+    private String mUserPassword;
+    private SharedPreferences mPreferences;
+    private static final String LOGIN_URL = "http://tranquil-atoll-5561.herokuapp.com/api/v1/sessions";
+
     public void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.login);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
+
     }
 
     public void login(View loginButton) {
-        Intent intent = new Intent(this, DashBoard.class);
-        startActivity(intent);
+        EditText userEmailField = (EditText) findViewById(R.id.et_userEmail);
+        mUserEmail = userEmailField.getText().toString();
+        EditText userPasswordField = (EditText) findViewById(R.id.et_userPassword);
+        mUserPassword = userPasswordField.getText().toString();
+        if (mUserEmail.length() == 0 || mUserPassword.length() == 0) {
+            // input fields are empty
+            Toast.makeText(this, "Please complete all the fields",
+                    Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            LoginTask loginTask = new LoginTask(this);
+            loginTask.setMessageLoading("Logging in...");
+            loginTask.execute(LOGIN_URL);
+
+        }
     }
 
-    public void register(View registerText)
-    {
+    public void register(View registerText) {
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
     }
 
+    private class LoginTask extends UrlJsonAsyncTask {
+        public LoginTask(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);
+            JSONObject holder = new JSONObject();
+            JSONObject userObj = new JSONObject();
+            String response = null;
+            JSONObject json = new JSONObject();
+
+            try {
+                try {
+                    json.put("success", false);
+                    json.put("info", "Something went wrong. Retry!");
+                    userObj.put("email", mUserEmail);
+                    userObj.put("password", mUserPassword);
+                    holder.put("user", userObj);
+                    StringEntity se = new StringEntity(holder.toString());
+                    post.setEntity(se);
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
+
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
+
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                    json.put("info",
+                            "Email and/or password are invalid. Retry!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("JSON", "" + e);
+            }
+
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if (json.getBoolean("success")) {
+                    SharedPreferences.Editor editor = mPreferences.edit();
+                    editor.putString("AuthToken", json.getJSONObject("data")
+                            .getString("auth_token"));
+                    editor.commit();
+
+                    finish();
+                    Intent intent = new Intent(getApplicationContext(),
+                            DashBoard.class);
+                    startActivity(intent);
+                }
+                Toast.makeText(context, json.getString("info"),
+                        Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG)
+                        .show();
+            } finally {
+                super.onPostExecute(json);
+            }
+        }
+
+    }
 }
