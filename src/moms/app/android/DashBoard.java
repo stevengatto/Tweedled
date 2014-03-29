2342a1ede6353584758219374d5182cd00efd748
lamdersar <klam@f1v.co>
2014-03-29 17:04:11 -0400

    auth token done

--- src/moms/app/android/DashBoard.java
+++ src/moms/app/android/DashBoard.java
@@ -1,14 +1,22 @@
 package moms.app.android;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by klam on 3/29/14.
  */
 public class DashBoard extends Activity {
+    private SharedPreferences mPreferences;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
+        TextView mAuth = (TextView)findViewById(R.id.auth);
+        String auth_token = mPreferences.getString("AuthToken","");
+        mAuth.setText(auth_token);
+
     }
 }
