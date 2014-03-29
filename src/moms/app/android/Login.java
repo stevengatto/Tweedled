0434fce106a0b699e4162cef731c1890fe267680
lamdersar <klam@f1v.co>
2014-03-29 14:12:39 -0400

    login page

---
+++ src/moms/app/android/Login.java
@@ -0,0 +1,15 @@
+package moms.app.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by klam on 3/29/14.
+ */
+public class Login extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.login);
+    }
+
+}
