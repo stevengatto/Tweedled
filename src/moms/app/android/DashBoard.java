09147dc2b556ec8ed807623bdda8bcb613da1639
lamdersar <klam@f1v.co>
2014-03-29 15:33:20 -0400

    login page with dashboard

---
+++ src/moms/app/android/DashBoard.java
@@ -0,0 +1,14 @@
+package moms.app.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by klam on 3/29/14.
+ */
+public class DashBoard extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
--- src/moms/app/android/Login.java
+++ src/moms/app/android/Login.java
@@ -1,7 +1,9 @@
 package moms.app.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by klam on 3/29/14.
@@ -10,6 +12,17 @@ public class Login extends Activity {
     public void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.login);
+    }
+
+    public void login(View loginButton) {
+        Intent intent = new Intent(this, DashBoard.class);
+        startActivity(intent);
+    }
+
+    public void register(View registerText)
+    {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 
 }
