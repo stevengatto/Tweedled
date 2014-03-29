aa0693e9f090859553922f62c124c436141ecaba
lamdersar <klam@f1v.co>
2014-03-29 14:11:54 -0400

    login page

--- src/moms/app/android/MyActivity.java
+++ src/moms/app/android/MyActivity.java
@@ -1,7 +1,11 @@
 package moms.app.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +15,16 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button changeButton = (Button)findViewById(R.id.but_changeview);
+        TextView textView = (TextView) findViewById(R.id.tvText);
+
+
     }
-}
+
+        public void login(View button) {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+        }
+
+    }
