ffd1cbfe6fe827a5a4f546f87e615969a8d363b7
steven57146 <steven.gatto001@umb.edu>
2014-04-30 12:55:27 -0400

    updated action bar with back nav

--- src/moms/app/android/ui/ImageSearchActivity.java
+++ src/moms/app/android/ui/ImageSearchActivity.java
@@ -1,16 +1,26 @@
 package moms.app.android.ui;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import moms.app.android.R;
 
 /**
  * Created by Steve on 4/26/14.
  */
-public class ImageSearchActivity extends BaseActivity {
+public class ImageSearchActivity extends Activity {
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.action_bar_bg));
+        actionBar.setLogo(R.drawable.action_bar_logo);
+        actionBar.setTitle("");
         setContentView(R.layout.image_search_activity);
     }
 }
--- src/moms/app/android/ui/ImageSelectedActivity.java
+++ src/moms/app/android/ui/ImageSelectedActivity.java
@@ -1,7 +1,10 @@
 package moms.app.android.ui;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.os.Bundle;import moms.app.android.R;
+import android.os.Bundle;
+import android.view.MenuItem;
+import moms.app.android.R;
 
 /**
  * Created by Steve on 4/27/14.
@@ -9,6 +12,11 @@ import android.os.Bundle;import moms.app.android.R;
 public class ImageSelectedActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.action_bar_bg));
+        actionBar.setLogo(R.drawable.action_bar_logo);
+        actionBar.setTitle("");
         setContentView(R.layout.image_selected_activty);
     }
 }
